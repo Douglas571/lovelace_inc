eefadf7e3cf97a68ff74c8bbeec606965887fb93
Douglas Socorro <juancarlossocorro571@gmail.com>
2010-05-06 04:32:22 -0430

    Cambio del motor de plantillas 'pug' por 'handlebars' en la configuracion del servidor.

--- Lovelace.js
+++ Lovelace.js
@@ -4,37 +4,24 @@ var express = require('express');
 var vhost 	= require('vhost');
 var router 	= require('./router.js');
 
-/*
- 
-var app = express()
-			.set('port', process.env.PORT || 3000)
-			.set('view engine', 'pug')
-			.set('views', './views');
-	
+
+var handlebars = require ('express-handlebars');
+handlebars.create({extname: '.hbs'});
+
+var app = express();
+
+//Engine configs
+app.engine('hbs', handlebars.engine);
+app.set('views', './views')
+	.set('view engine', 'hbs');
+
+// Middlewar configs
 app.use(express.static(__dirname + "/public"));			
 app.use(vhost('admin.*', router.admin));
 app.use(router.user);
-*/
 
-
-
-var hbs = require ('express-handlebars')
-							.create({extname: '.hbs'});
-
-var app = express();
-app.set('port', 3000);
-
-app.engine('hbs', hbs.engine);
-app.set('view engine', 'hbs');
-
-app.get('/hora', function(req, res){
-	res.send('18-06-2020');
-});
-
-app.get('/', function(req, res){
-	res.render('home', {name: 'Douglas'});
-});
-
+//set up the server
+app.set('port', process.env.PORT || 3000)
 app.listen(app.get('port'), function(){
 	console.log(`Lovelace is launched in: ` + 
 				`http://localhost:${app.get('port')}`);
