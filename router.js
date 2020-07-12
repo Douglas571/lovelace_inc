4991bb291a9d57b62f2d6c31821d6ff43c4f1c2d
Douglas Socorro <juancarlossocorro571@gmail.com>
2020-07-12 14:19:30 -0430

    some changes in the files and restored the 'router.js'

---
+++ router.js
@@ -0,0 +1,20 @@
+'use strict';
+let admin = require('./handlers/admin.js');
+let main = require('./handlers/main.js');
+
+let userRoutes = require('express').Router();
+let adminRouts = require('express').Router();
+
+userRoutes
+   .get('/about', main.about)
+   .get('/contact', main.contact)
+   .get('/portfolio', main.portfolio)
+   .get('/services', main.services)
+   .get('/', main.home);
+
+adminRouts
+   .get('/', admin.home)
+   .use('/blogUpdate', admin.blogUpdate);
+
+exports.user = userRoutes;
+exports.admin = adminRouts;
