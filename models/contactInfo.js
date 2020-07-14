7f66014d6ec76392c55b9189c9b56d107c1220db
Douglas Socorro <juancarlossocorro571@gmail.com>
2020-07-14 16:45:52 -0430

    refactoring without pass to DB

--- models/contactInfo.js
+++ models/contactInfo.js
@@ -1,9 +1,17 @@
-let { writeFile , readFileSync} = require('fs');
-
-function set(info, callback){
-
+const { writeFile , readFileSync} = require('fs');
+const path = {
+	contactInfo: `${__dirname}/contactInfo.json`,
 }
 
-function get(callback){
-	
+exports.set = function(info, callback){
+	info = JSON.stringify(info);
+	writeFile(path.contactInfo, info, callback);
+}
+
+exports.get = function(){
+	let contactInfoString = readFileSync(path.contactInfo, 'utf-8');
+	//TO-DO: implement readFileSync error handler.
+
+	let contactInfo = JSON.parse(contactInfoString);
+	return contactInfo;
 }
