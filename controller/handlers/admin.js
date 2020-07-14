abd48a1a57e599f59c40f32ceae1e1597c27a8d7
Douglas Socorro <juancarlossocorro571@gmail.com>
2020-07-14 14:36:09 -0430

    Progress in the funcionality, is working, but need refactoring.

--- controller/handlers/admin.js
+++ controller/handlers/admin.js
@@ -1,11 +1,11 @@
+'use strict';
+
+let { writeFile , readFileSync} = require('fs');
 let BlogEntry = require("./../../models/blogEntry.js");
-
-
-'use strict'
 
 exports.home = function(req, res){
 	res.send('Hello Admin!');
-};
+}
 
 exports.blogUpdate = function(req, res){
 	switch(req.method){
@@ -14,6 +14,7 @@ exports.blogUpdate = function(req, res){
 			console.log(req.url);
 			res.render('blogAdmin');
 			break;
+
 		case "POST":
 			console.log("POST - Entrada al blog");
 			console.log(req.url);
@@ -28,12 +29,26 @@ exports.blogUpdate = function(req, res){
 				console.log("save entry:");
 				console.log(ent);
 			});
-
-
 			break;
+
 		default:
 			console.log("Metodo desconocido");
 	}	
+}
+
+function getNewContactInfo(req){
+	//TO-DO: Parse the contact info from request here.
+	console.log(req.headers['content-type'] + ":");
+	console.log(req.body);
+	return req.body;
+}
+ 
+function setNewContactInfo(newContactInfo, callback){
+	newContactInfo = JSON.stringify(newContactInfo);
+	let content = readFileSync('./controller/handlers/contactInfo.json', 'utf-8');
+	console.log('old content is: \n' + content);
+	writeFile('./controller/handlers/contactInfo.json', newContactInfo, callback);
+
 }
 
 exports.contact = function(req, res){
@@ -44,6 +59,23 @@ exports.contact = function(req, res){
 		case 'GET':
 			res.render('adminContact', { phone: '0412-162-65-58' });
 			break;
+
+		case 'POST':
+			//TO-DO: Put function to edit contact info here.
+			let newContactInfo = getNewContactInfo(req);
+			setNewContactInfo(newContactInfo, (err) => {
+				if(err) {
+					console.warn(err);
+					res.send({success: false});
+				}
+
+				let content = readFileSync('./controller/handlers/contactInfo.json', 'utf-8');
+				console.log('new Content is: \n' + content);
+				res.set("content-type", "application/json");
+				res.send({success: true});
+			});
+			break;
+
 		default:
 			res.send(`Method: ${req.method} not alowed or available.`);
 	}
